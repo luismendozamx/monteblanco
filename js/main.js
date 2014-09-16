135d5ec4470ef8c246e8e90b52c3481ce785d414
Luis Mendoza <luis@mendoza.io>
2014-09-16 01:13:35 -0500

    Added shadows and full screen

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+$(document).ready( function(){
 
+	//Main container screen height
+	var windowHeight = $(window).height();
+	$("#secciones").css("margin-top", (windowHeight*.8) + "px");
+	$("#fixed-background").height(windowHeight);
+});
