c14d6cbcd906254df0d17e5c3496b6905037a0d6
Luis Mendoza <luis@mendoza.io>
2014-09-17 01:14:28 -0500

    Added fonts and secondary page structure.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ $(document).ready( function(){
 
 	//Main container screen height
 	var windowHeight = $(window).height();
-	$("#secciones").css("margin-top", (windowHeight*.8) + "px");
+	$("#secciones-home").css("margin-top", (windowHeight*.8) + "px");
 	$("#fixed-background").height(windowHeight);
 });
