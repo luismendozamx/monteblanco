f27b443b565202c8cc569d4fb4c56d5e1308a75d
Luis Mendoza <luis@mendoza.io>
2014-10-19 19:12:12 -0500

    Optimized CSS and Page Layout

--- js/main.js
+++ js/main.js
@@ -2,33 +2,39 @@ $(document).ready( function(){
 
 	//Main container screen height
 	var windowHeight = $(window).height();
+	var windowWidth = $(window).width();
 
-	//Make Full-Screen and adjust element pposition
+	//Make Full-Screen and adjust element position
 	$("#slider").css("margin-top", (windowHeight/2)-120 + "px");
 	$("#nav-and-slider").height(windowHeight-20);
 	$("#secciones-home").css("margin-top", "0px");
 	$('#scroll-down').css("margin-top", (windowHeight/4)-40 + "px");
-	$("#fixed-background").height(windowHeight);
+
+	if( windowWidth > 800){
+		$("#fixed-background").height(windowHeight);
+	}else{
+		$("#fixed-background").height(windowHeight);
+	}
 
 	//Start Slideshow
 	if( $("#fixed-background") != undefined){
-		$("#fixed-background").backstretch([
-		"img/home-2.jpg",
-		"img/Fondo1.jpg"
-		], {duration: 4000, fade: 750} );
+	  $("#fixed-background").backstretch([
+	  "img/home-2.jpg",
+	  "img/Fondo1.jpg"
+	  ], {duration: 4000, fade: 750} );
 	}
-	
+
 	//Slideshow controllers
 	$("#prev").click( function(){
-		$("#fixed-background").backstretch("prev");
+	  $("#fixed-background").backstretch("prev");
 	});
 	$("#next").click( function(){
-		$("#fixed-background").backstretch("next");
+	  $("#fixed-background").backstretch("next");
 	});
 
 	//Make Local Scroll
 	$.localScroll();
-  	$('#slider').localScroll({ duration: 'slow', offset: 0});
+	$('#slider').localScroll({ duration: 'slow', offset: 0});
 });
 
 $(window).resize(function(){
---
+++ js/page.js
@@ -0,0 +1,8 @@
+$(document).ready( function(){
+
+	//Main container screen height
+	var windowHeight = $(window).height();
+	var windowWidth = $(window).width();
+
+	
+});
