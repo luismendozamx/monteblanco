a2cbc25c0c1dd8761ef69dbee50b2c0d33f3c167
Luis Mendoza <luis@mendoza.io>
2014-10-27 19:57:55 -0600

    Optimised home page responsive layout

--- js/main.js
+++ js/main.js
@@ -1,24 +1,7 @@
 $(document).ready( function(){
 
-	//Main container screen height
-	var windowHeight = $(window).height();
-	var windowWidth = $(window).width();
-
-	//Make Full-Screen and adjust element position
-	$("#slider").css("margin-top", (windowHeight/2)-120 + "px");
-	if( windowWidth < 768 ){
-		$("#nav-and-slider").height(windowHeight);
-	}else{
-		$("#nav-and-slider").height(windowHeight-20);
-	}
-	$("#secciones-home").css("margin-top", "0px");
-	$('#scroll-down').css("margin-top", (windowHeight/4)-40 + "px");
-
-	if( windowWidth > 800){
-		$("#fixed-background").height(windowHeight);
-	}else{
-		$("#fixed-background").height(windowHeight);
-	}
+	//Adjust Object position and size.
+	resizeObjects();
 
 	//Start Slideshow
 	if( $("#fixed-background") != undefined){
@@ -42,12 +25,31 @@ $(document).ready( function(){
 });
 
 $(window).resize(function(){
-	var windowHeight = $(window).height();
-
-	//Make Full-Screen and adjust element pposition
-	$("#slider").css("margin-top", (windowHeight/2)-120 + "px");
-	$("#nav-and-slider").height(windowHeight-20);
-	$("#secciones-home").css("margin-top", "0px");
-	$('#scroll-down').css("margin-top", (windowHeight/4)-40 + "px");
-	$("#fixed-background").height(windowHeight);
+	//Adjust Object position and size.
+	resizeObjects();
 });
+
+function resizeObjects(){
+	//Main container screen height
+	var windowHeight = $(window).height();
+	var windowWidth = $(window).width();
+
+	//Make Full-Screen and adjust element position
+	$("#slider").css("margin-top", (windowHeight/2)-120 + "px");
+	if( windowWidth < 768 ){
+		$("#nav-and-slider").height(windowHeight);
+	}else{
+		$("#nav-and-slider").height(windowHeight-20);
+	}
+	$("#secciones-home").css("margin-top", "0px");
+
+	position = $('#main-nav').height() + 40 + ((windowHeight/2)-120) + $("#slider").height() + $('#scroll-down').height();
+
+	$('#scroll-down').css("margin-top", windowHeight - position + "px");
+ 
+	if( windowWidth > 800){
+		$("#fixed-background").height(windowHeight);
+	}else{
+		$("#fixed-background").height(windowHeight);
+	}
+}
--- js/page.js
+++ js/page.js
@@ -3,6 +3,18 @@ $(document).ready( function(){
 	//Main container screen height
 	var windowHeight = $(window).height();
 	var windowWidth = $(window).width();
+	sideMenuWidth = $('#side-menu').width();
 
+	//$('#side-menu-container').width( ($('#page-content').width())/3 );
 	
+});
+
+$(document).on('scroll', function(){
+
+	if( $('#side-menu').hasClass('isStuck') ){
+		$('#side-menu').width( (($('#page-content').width())/3) - 30 );
+	}else{
+		$('#side-menu').width( sideMenuWidth );
+	}
+
 });
